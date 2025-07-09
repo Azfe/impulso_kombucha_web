bf86ec76fb08aa4af6dc91beb01c0ccfd02a73a8
Alex Zapata <alexzapata1984@gmail.com>
2025-07-10 01:18:56 +0200

    Añadir animación al header cuando se hace scroll

--- js/navbar.js
+++ js/navbar.js
@@ -9,3 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// JavaScript para manejar el scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.impulso-navbar');
+  const logoContainer = document.querySelector('.impulso-logo-container');
+  const logo = document.querySelector('.impulso-logo');
+  const pinya1 = document.querySelector('.img-pinya');
+  const pinya2 = document.querySelector('.img-pinya-2');
+
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+    logoContainer.classList.add('scrolled');
+    logo.classList.add('scrolled');
+    pinya1.classList.add('scrolled');
+    pinya2.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+    logoContainer.classList.remove('scrolled');
+    logo.classList.remove('scrolled');
+    pinya1.classList.remove('scrolled');
+    pinya2.classList.remove('scrolled');
+  }
+});
